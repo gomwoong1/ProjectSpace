738a5d3f7c378abb3f8eb7d88c350307854a5a24
gomwoong1 <gomwoong0619@naver.com>
2022-10-24 15:54:13 +0900

    test

--- TCP/src/ServerFrame.java
+++ TCP/src/ServerFrame.java
@@ -1,18 +1,22 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-//		BufferedReader in = null;
-//		BufferedWriter out = null;
-//		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//		out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 public class ServerFrame {
+	
 	public static void main(String[] args) {
+		
+		ServerFrame serverFrame = new ServerFrame();
+		serverFrame.start();
+	}
+	
+	public void start() {
 		ServerSocket server = null;
 		Socket socket = null;
 		try {
@@ -21,35 +25,73 @@ public class ServerFrame {
 				System.out.println("[ 클라이언트 접속 대기중 ]");
 				socket = server.accept();
 				
-				String inMsg = in.readLine();
-				System.out.println(" >> 클라이언트 : " + inMsg);
-				
-				if(inMsg.equalsIgnoreCase("bye")) { //대소문자 구분 X
-					System.out.println("클라이언트가 종료되었습니다.");
-					break;
-				}
-				
-				String outMsg = sc.nextLine();
-				out.write(outMsg + "\n");
-				out.flush();
-				
+				ServerThread reciveThread = new ServerThread(socket);
+				reciveThread.start();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				out.close();
-				in.close();
+				System.out.println("[ 서버 종료 ]");
 				socket.close();
 				server.close();
-				sc.close();
 			} catch (IOException e) {
+				System.out.println("[ 서버 통신 에러 ]");
 				e.printStackTrace();
 			}
 		}
 	}
+}
+
+class ServerThread extends Thread{
+	static List<PrintWriter> list = Collections.synchronizedList(new ArrayList<PrintWriter>());
 	
-	public class ServerThread extends Thread{
-		
+	Socket socket = null;
+	BufferedReader in = null;
+	PrintWriter out = null;
+	
+	public ServerThread(Socket socket) {
+		this.socket = socket;
+		try {
+			out = new PrintWriter(socket.getOutputStream());
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			list.add(out);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		String name = "";
+		try {
+			name = in.readLine();
+			System.out.println("[" + name + "님 새연결 생성");
+			sendAll("[" + name + "]님이 들어오셨습니다.");
+			
+			while(in != null) {
+				String inputMsg = in.readLine();
+				if("quit".equals(inputMsg))	break;
+				sendAll(name + ">>" + inputMsg);
+			}
+		} catch (IOException e) {
+			System.out.println("[" + name + " 접속끊김]");
+		} finally {
+			sendAll("[" + name + "]님이 나가셨습니다.");
+			list.remove(out);
+			try {
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("[" + name + "연결종료]");
+	}
+	
+	private void sendAll (String s) {
+		for (PrintWriter out: list) {
+			out.println(s);
+			out.flush();
+		}
 	}
 }
+
