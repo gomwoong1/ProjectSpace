6b463dddf8f68aaa4d897eaa15dab36ba5078a63
Seungwan Han <hseungwan@naver.com>
2022-10-29 21:24:21 +0900

    급하게 푸시

--- TCP/src/project/ranking.java
+++ TCP/src/project/ranking.java
@@ -7,11 +7,13 @@ import java.awt.Font;
 import java.awt.datatransfer.ClipboardOwner;
 import java.security.KeyStore.PrivateKeyEntry;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 import javax.xml.stream.events.Comment;
 
@@ -19,11 +21,11 @@ public class ranking extends JFrame{
 
 	private Font mainFont;
 	private Font subFont;
+	private Font inputFont;
 	private JTextArea firstrank;
 	private JTextArea secondrank;
 	private JTextArea thirdrank;
 	
-
 public ranking() {
 	setTitle("Ranking");
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -33,7 +35,8 @@ public ranking() {
 	
 	mainFont = new Font("ROKAF SLAB SERIF MEDIUM", Font.BOLD, 50);
 	subFont = new Font("ROKAF SLAB SERIF MEDIUM", 0, 19);
-
+	inputFont = new Font("ROKAF SLAB SERIF MEDIUM", 0, 14);
+	
 	setUp();
 	setDown();
 	
@@ -49,6 +52,27 @@ public void setUp() {
 	JLabel step = new JLabel(new ImageIcon("image/step.png"));
 	step.setBounds(50,30,388,300);
 	up.add(step);
+	
+	firstrank = new JTextArea("1µî:",1,15);
+	firstrank.setEditable(false);
+	firstrank.setFont(inputFont);
+	firstrank.setBounds(175, 150, 50, 20);
+	firstrank.setBorder(BorderFactory.createEmptyBorder());
+	up.add(firstrank);
+	
+	secondrank = new JTextArea("2µî:",1,15);
+	secondrank.setEditable(false);
+	secondrank.setFont(inputFont);
+	secondrank.setBounds(55, 220, 50, 20);
+	secondrank.setBorder(BorderFactory.createEmptyBorder());
+	up.add(secondrank);
+	
+	thirdrank = new JTextArea("2µî:",1,15);
+	thirdrank.setEditable(false);
+	thirdrank.setFont(inputFont);
+	thirdrank.setBounds(325, 220, 50, 20);
+	thirdrank.setBorder(BorderFactory.createEmptyBorder());
+	up.add(thirdrank);
 	
 	add(up, BorderLayout.NORTH);
 	
