c3c6b66e884fbaa424eed6dcc99181363d084b4f
gomwoong1 <gomwoong0619@naver.com>
2022-11-24 07:05:44 +0900

    1124 작업
    - 타이머 기능 완성
    - 지속적으로 기능 붙여나갈 예정

--- record.c
+++ record.c
@@ -3,6 +3,10 @@
 #include <pthread.h>
 #include <unistd.h>
 
+// 타이핑 계속 받아서 기록조회, 일시정지 기능 쓸 수 있게.
+// 기록 시작할 때 받아온 타임값이 00:00:00이 아니라면 지속해서 기록할건지
+// 아니면 새로 시작할건지 묻는 여부도
+
 void* timer();
 
 int main(){
@@ -15,12 +19,27 @@ int main(){
 }
 
 void* timer(){ 
-    int i = 0;
+    int hour = 0, min = 59, sec = 0;
+
+    system("clear");
+    printf("00:00:00\n");
+
     // 1초마다 시간 출력하기
     while(1){
         sleep(1);
-        i++;
-        printf("child: %d초\n", i);
+        sec++;
+
+        if((sec%60) == 0){
+            min++;
+            sec = 0;
+            if((min%60) == 0){
+                hour++;
+                min = 0;
+            }
+        }
+            
+        printf("\x1b[%d;%dH", 1,1);
+        printf("%02d:%02d:%02d\n", hour, min, sec);
     }
     pthread_exit(NULL);
 }
