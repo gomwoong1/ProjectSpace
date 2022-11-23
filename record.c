70d7bbd8f87ef5eb4c7c07548748f3748c1723b5
gomwoong1 <gomwoong0619@naver.com>
2022-11-24 04:07:10 +0900

    1124 작업중
    - 1초마다 시간 출력 성공
    - 1초마다 값 더해서 타이머 출력해보기

--- record.c
+++ record.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
-void* timer(void *);
+void* timer();
 
 int main(){
     pthread_t thread;
 
-    pthread_create(&thread, NULL, timer,NULL);
+    pthread_create(&thread, NULL, timer, NULL);
     pthread_join(thread, NULL);
+
     return 0;
 }
 
-void* timer(void* arg){
-    
+void* timer(){ 
+    int i = 0;
+    // 1초마다 시간 출력하기
+    while(1){
+        sleep(1);
+        i++;
+        printf("child: %d초\n", i);
+    }
     pthread_exit(NULL);
 }
