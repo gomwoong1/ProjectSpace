558d38ee2eded6f37627fabd361125bf32f608d8
gomwoong1 <gomwoong0619@naver.com>
2022-12-05 11:16:56 +0900

    DB Insert 이전에 결과를 찍는 문제 해결

--- TCP/src/project/gameFrame.java
+++ TCP/src/project/gameFrame.java
@@ -199,6 +199,11 @@ public class gameFrame extends JFrame implements ActionListener{
 					timer.cancel();
 					DB db = new DB();
 					db.resultInsert(username, score);
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 					callProgram();
 				}
 			}
